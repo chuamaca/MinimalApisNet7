29af6d735b4f6b48c3b3f5376a8523873c48f05c
chuamaca <cesarhuamanicastro@gmail.com>
2023-10-23 12:02:07 -0500

    minimal Api basic

---
+++ MinimalApi/Todo.cs
@@ -0,0 +1,10 @@
+﻿namespace MinimalApi
+{
+    public class Todo
+    {
+
+        public int Id { get; set; }
+        public String? Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
